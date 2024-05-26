5bafd52c4f61b1b6af1035833b7c2d52c5f13f0d
Alex Kharitonov <alexkharitonov.unvml@gmail.com>
2024-05-27 01:06:12 +0300

    Added new textures. And fix bullets

--- scripts/bullet.cs
+++ scripts/bullet.cs
@@ -5,18 +5,41 @@ public partial class bullet : Node3D
 {
 	const float Speed = 40.0f;
 
+    [Export]
+    public MeshInstance3D BulletMash { get; set; }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    [Export]
+	public RayCast3D BulletRayCast { get; set; }
+	[Export]
+	public GpuParticles3D Particles { get; set; }
+    [Export]
+    public Timer Timer { get; set; }
+
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
+        Timer.Timeout += OnTimerTimeout;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void OnTimerTimeout()
+    {
+        this.QueueFree();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
         var fdelta = (float)delta;
 
-        var vec = new Vector3(0, 0, Speed) * fdelta;
+        var vec = new Vector3(0, 0, -Speed) * fdelta;
         Position += Transform.Basis * vec;
+		if (BulletRayCast.IsColliding())
+		{
+			BulletMash.Visible = false;
+			Particles.Emitting = true;
+		}
     }
+
+	
 }
