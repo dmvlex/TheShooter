8fc98f0611775db1a617517eb9cb672e3e01cc4b
Alex Kharitonov <alexkharitonov.unvml@gmail.com>
2024-05-27 21:28:27 +0300

    finalize project

--- scripts/GUI.cs
+++ scripts/GUI.cs
@@ -11,7 +11,11 @@ public partial class GUI : Control
     [Export]
     public int AmmoMags { get; set; } = 5;
     public int CurrentAmmo { get; set; }
-	[Export]
+    [Export]
+    public Label ResultLabel { get; set; }
+    [Export]
+    public Label PointsLabel { get; set; }
+    [Export]
 	public Label PointsOutput { get; set; }
     [Export]
     public Label Warning { get; set; }
@@ -27,6 +31,65 @@ public partial class GUI : Control
 		Warning.Visible = false;
 	}
 
+	private void SetHudVisibility(bool visible)
+	{
+		PointsOutput.Visible = visible;
+        Warning.Visible = visible;
+        AmmoOutput.Visible = visible;
+        MagsOutput.Visible = visible;
+		SetWarningVisibility(visible);
+		PointsLabel.Visible = visible;
+    }
+
+	private void ResetGame()
+	{
+		CurrentAmmo = 0;
+		Points = 0;
+		AmmoMags = 6;
+	}
+
+	private void SetResultVisibility(bool visible)
+	{
+        ResultLabel.Visible = visible;
+		if (visible)
+		{
+            ResultLabel.Text = "";
+            ResultLabel.Text = $"Your score: {Points} \n";
+            ResultLabel.Text += "Press [Q] to try again\nPress [ESC] to quit\n";
+        }
+    }
+
+	private void GetGameResult()
+	{
+		if (AmmoMags == 0 && CurrentAmmo == 0)
+		{
+			SetHudVisibility(false);
+			SetResultVisibility(true);
+
+			if (Input.IsActionJustPressed("Restart"))
+			{
+				SetResultVisibility(false);
+                SetHudVisibility(true);
+                ResetGame();
+			}
+		}
+		else
+		{
+			SetHudVisibility(true);
+		}
+	}
+	private void SetWarningVisibility(bool visible)
+	{
+		if (visible)
+		{
+            if (CurrentAmmo > 0) Warning.Visible = false;
+            else Warning.Visible = true;
+		}
+		else
+		{
+			Warning.Visible = visible;
+		}
+	}
 	private void PrintMags()
 	{
         MagsOutput.Text = "";
@@ -39,15 +102,9 @@ public partial class GUI : Control
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		if (CurrentAmmo > 0)
-		{
-            Warning.Visible = false;
-        }else
-		{
-			Warning.Visible = true;
-		}
+		GetGameResult();
 
-		PrintMags();
+        PrintMags();
 
 		PointsOutput.Text = Points.ToString();
 		AmmoOutput.Text = $"{CurrentAmmo}/{MaxAmmo}";
